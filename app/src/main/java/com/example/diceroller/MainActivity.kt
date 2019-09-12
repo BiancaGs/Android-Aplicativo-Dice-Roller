d23dfcdd258672940b741814440933d354b29fc6
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-09-12 13:53:00 -0300

    Adição da função rollDice()

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,8 +16,14 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
         rollButton.setOnClickListener{
-            Toast.makeText( this, "Button Clicked", Toast.LENGTH_SHORT).show()
-            /* Duração LENGTH_SHORT or LENGTH_LONG*/
+            rollDice()
+
         }
+    }
+
+    private fun rollDice(){
+        val resultText: TextView = findViewById(R.id.result_text)
+//        Set the   text
+        resultText.text = "Dice rolled!"
     }
 }
